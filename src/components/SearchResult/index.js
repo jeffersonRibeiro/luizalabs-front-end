bbaa93185a6fe4680c9e7aa2eb25c044020b4193
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2019-01-19 20:39:40 -0200

    Close search button working

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import IosHeartOutline from 'react-ionicons/lib/IosHeartOutline';
+import MdClose from 'react-ionicons/lib/MdClose';
 
 import Place from '../Place';
 import { isObjectEmpty } from '../../services/util';
 
-const SearchResult = ({ searchResult }) => {
-  if (isObjectEmpty(searchResult)) {
-    return null;
+import './style.scss';
+
+class SearchResult extends Component {
+  state = {
+    isResultVisible: true
+  };
+
+  closeResult = () => {
+    this.setState({ isResultVisible: false });
+  };
+
+  componentWillUpdate(nextProps) {
+    if (this.props.searchResult !== nextProps.searchResult) {
+      this.setState({ isResultVisible: true });
+    }
   }
 
-  return (
-    <React.Fragment>
-      <Place place={searchResult} />
-    </React.Fragment>
-  );
-};
+  render() {
+    let { searchResult } = this.props;
+    const { isResultVisible } = this.state;
+
+    searchResult = {
+      city: 'São Paulo - SP',
+      neighborhood: 'Vila Indiana',
+      street: 'Rua Abadia dos Dourados',
+      zipCode: '05586-030'
+    };
+
+    if (isObjectEmpty(searchResult) || !isResultVisible) {
+      return null;
+    }
+
+    return (
+      <div className="search-result">
+        <div className="search-result-actions">
+          <button>
+            <IosHeartOutline />
+          </button>
+          <button onClick={this.closeResult}>
+            <MdClose />
+          </button>
+        </div>
+        <Place place={searchResult} />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   searchResult: state.search.result
