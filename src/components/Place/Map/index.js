7e8ccff50e794548efa0494a3c965c7b91e5aa22
tvglobo <jefferson.ribeiro.contato@gmail.com>
2019-01-21 15:27:24 -0200

    Loading indicator for maps

--- src/components/Place/Map/index.js
+++ src/components/Place/Map/index.js
@@ -9,12 +9,13 @@ the Google Static Map API and protect my API_KEY
 */
 
 const Map = ({ zipCode, alt, title }) => (
-  <img
-    className="google-map"
-    alt={alt}
-    title={title}
-    src={`https://luiza-labs-static-map.herokuapp.com/${zipCode}`}
-  />
+  <div class="google-map">
+    <img
+      alt={alt}
+      title={title}
+      src={`https://luiza-labs-static-map.herokuapp.com/${zipCode}`}
+    />
+  </div>
 );
 
 Map.propTypes = {
